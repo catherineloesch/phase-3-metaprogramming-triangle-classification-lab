ed34d8ebe2dc322fb09f5777dc9628581bdc7b4b
Catherine Loesch <catherine.loesch@protonmail.com>
2021-10-18 22:17:33 +0100

    assignment completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+
+  attr_accessor :sides, :a, :b, :c
+
+  def initialize(a, b, c)
+    @sides = [a, b, c]
+    @a = a
+    @b = b
+    @c = c
+
+  end
+
+  def kind
+    if !self.valid_triangle? 
+      raise TriangleError 
+    elsif self.sides.uniq.length == 1
+        :equilateral
+    elsif self.sides.uniq.length == 2
+        :isosceles
+    else
+        :scalene
+    end
+  end
+
+  def valid_triangle?
+    if self.sides.filter {|side| side > 0 }.length != 3
+      false
+    elsif !(a + b > c && a + c > b && b + c > a)
+      false
+    else
+      true
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
